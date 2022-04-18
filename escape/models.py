0851b0b3e91140ac5a4f260f996e133c9f9f75a7
Zainab Shah <zainabshah2003@gmail.com>
2022-04-19 00:55:07 +0300

    Game save function completed

--- escape/models.py
+++ escape/models.py
@@ -9,8 +9,8 @@ class User(AbstractUser):
 class Profile(models.Model):
     gamer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="game_user")
     score = models.IntegerField(default=0, null=True, blank=True)
-    progress = models.CharField(max_length=100)
+    progress = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"User {self.gamer}:{self.score}"
+        return f"User {self.id}:{self.gamer}"
 
--- escape/views.py
+++ escape/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 from .models import Profile, User
 
 
 def index(request):
-    return render(request, 'escape/index.html')
+    
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(gamer = request.user)
+        id = profile.id
+    else:
+        id = -1
+    
+
+    return render(request, 'escape/index.html', {
+        'id': id
+    })
 
 
 def login_view(request):
@@ -61,7 +73,38 @@ def register(request):
             return render(request, "escape/register.html", {
                 "message": "Username already taken."
             })
+
+        profile = Profile(gamer = user)
+        profile.save()
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "escape/register.html")
+
+def save(request,id):
+
+    # Get the profile of user whose progress has to be saved or retrieved
+    try:
+        profile = Profile.objects.get(gamer=request.user)
+
+    except User.DoesNotExist:
+        return JsonResponse({
+            "error": "Profile does not exist."
+        }, status=400)
+
+    # Return a number indicating user's progress
+    if request.method=='GET':
+        savenum = profile.progress
+        # if request.user.is_authenticated:
+        #     pass
+        return JsonResponse({
+            "savenum": savenum
+        })
+
+    # Put a number indicating user's progress
+    if request.method=='POST':
+        res = json.loads(request.body)
+        savenum = res['data']
+        profile.progress = savenum
+        profile.save()
+        return HttpResponse(status=204)
